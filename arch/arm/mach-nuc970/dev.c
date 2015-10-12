805d8498081dcb09c6abed4e0d1b74d056dca1e2
schung <schung@nuvoton.com>
2015-10-12 11:41:06 +0800

    Add select RGB888/RGB565 on 800x480 5-Inch Color TFT LCD
    Change-Id: I061ba28c15222ac489ed461e180186aaa83eb1f7

--- arch/arm/mach-nuc970/dev.c
+++ arch/arm/mach-nuc970/dev.c
@@ -324,12 +324,17 @@ static struct nuc970fb_display nuc970fb_lcd_info[] = {
 #ifdef CONFIG_E50A2V1_800X480
 	/* E50A2V1 800x480 TFT Panel , 24bits*/
 	[0] = {
+		#if  defined(CONFIG_SRCFMT_RGB888)
 		.type		= LCM_DCCS_VA_SRC_RGB888,
+		.bpp		= 32,
+		#elif defined(CONFIG_SRCFMT_RGB565)
+		.type   = LCM_DCCS_VA_SRC_RGB565,
+		.bpp		= 16,
+		#endif
 		.width		= 800,
 		.height		= 480,
 		.xres		= 800,
 		.yres		= 480,
-		.bpp		= 32,
 		.pixclock	= 200000,
 		.left_margin	= 88,
 		.right_margin   = 40,
@@ -337,9 +342,14 @@ static struct nuc970fb_display nuc970fb_lcd_info[] = {
 		.upper_margin	= 32,
 		.lower_margin	= 13,
 		.vsync_len		= 3,
-		.dccs		= 0x0e00020a,
+		#if  defined(CONFIG_SRCFMT_RGB888)
+		.dccs		= 0x0e00020a,				
+	  .fbctrl		= 0x03200320,				
+		#elif defined(CONFIG_SRCFMT_RGB565)
+		.dccs		= 0x0e00040a,
+		.fbctrl		= 0x01900190,
+		#endif				
 		.devctl		= 0x070000c0,
-		.fbctrl		= 0x03200320,
 		.scale		= 0x04000400,
 	},
 #endif
