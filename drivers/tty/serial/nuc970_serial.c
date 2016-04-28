acc632d57bc419047deee633c5addb0372463ce9
Yi-An Chen <yachen@nuvoton.com>
2016-04-28 09:39:00 +0800

    [UART] Fix UART2 MFP config naming issue
    Config name in driver source didn't match the naming in Kconfig.
    Update driver so the MFP can be set correctly
    
    Change-Id: If7ab28cd1a1628357f85bfb6e47dd30f47bcd014

--- drivers/tty/serial/nuc970_serial.c
+++ drivers/tty/serial/nuc970_serial.c
@@ -898,9 +898,9 @@ if(pdev->id == 1)
 }
 else if(pdev->id == 2)
 {
-#if defined (CONFIG_NUC970_UART2)
+#if defined (CONFIG_NUC970_UART2_PF)
 	p = devm_pinctrl_get_select(&pdev->dev, "uart2");
-#elif defined (CONFIG_NUC970_UART2_FC)
+#elif defined (CONFIG_NUC970_UART2_PF_FC)
 	p = devm_pinctrl_get_select(&pdev->dev, "uart2_fc");
 #endif
 
