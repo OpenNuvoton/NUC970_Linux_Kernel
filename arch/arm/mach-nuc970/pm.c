048b84f3afd4b2c6b22928920e303b7b17d216a4
schung <schung@nuvoton.com>
2016-08-25 09:13:27 +0800

    Add execution of power down code from SRAM
    Change-Id: Ie2e496d4a63801bc7a653dae41567e55d759721b

--- arch/arm/mach-nuc970/pm.c
+++ arch/arm/mach-nuc970/pm.c
@@ -24,16 +24,58 @@
 #include <mach/map.h>
 
 #ifdef CONFIG_PM_SLEEP
+#define PM_FROM_SRAM
 
+#ifdef PM_FROM_SRAM
+#include <asm/cacheflush.h>
+
+#define SRAM_BASE	NUC970_VA_SRAM
+#define SRAM_LEN	NUC970_SZ_SRAM
+#define TEMP_SRAM_AREA  (SRAM_BASE+0x7000)
+/*
+ * Pointers used for sizing and copying suspend function data
+ */
+extern int nuc970_sys_suspend(void);
+extern int nuc970_sys_suspend_sz;
+#endif
 
 static int nuc970_suspend_enter(suspend_state_t state)
 {
+
+	#ifdef PM_FROM_SRAM
+	int (*nuc970_suspend_ptr) (void);
+	void *sram_swap_area;
+	#endif
 
 	if(state != PM_SUSPEND_MEM)
 		return -EINVAL;
 
 	__raw_writel(__raw_readl(REG_CLK_PMCON) & ~1, REG_CLK_PMCON);	// clear bit 0 so NUC970 enter pd mode instead of idle in next function call
-	cpu_do_idle();
+	#ifndef PM_FROM_SRAM
+		cpu_do_idle();
+	#else
+	/* Allocate some space for temporary SRAM storage */
+	sram_swap_area = kmalloc(nuc970_sys_suspend_sz, GFP_KERNEL);
+	if (!sram_swap_area) {
+		printk(KERN_ERR"PM Suspend: cannot allocate memory to save portion of SRAM\n");
+		return -ENOMEM;
+	}
+
+	/* Backup a small area of SRAM used for the suspend code */
+	memcpy(sram_swap_area, (void *) TEMP_SRAM_AREA,nuc970_sys_suspend_sz);
+	/*
+	 * Copy code to suspend system into SRAM.
+	 * The suspend code needs to run from SRAM.
+	 */
+	memcpy(TEMP_SRAM_AREA, (void *) nuc970_sys_suspend,nuc970_sys_suspend_sz);
+	flush_icache_range((unsigned long)TEMP_SRAM_AREA,(unsigned long)(TEMP_SRAM_AREA) + nuc970_sys_suspend_sz);
+	nuc970_suspend_ptr = (void *) TEMP_SRAM_AREA;
+	flush_cache_all();
+	(void) nuc970_suspend_ptr();
+
+	/* Restore original SRAM contents */
+	memcpy((void *) TEMP_SRAM_AREA, sram_swap_area,nuc970_sys_suspend_sz);
+	#endif
 	
 	__raw_writel(__raw_readl(REG_WKUPSER), REG_WKUPSSR);	// clear wake source flag
 
