ea82ac8379dfe630dc35e0d96829700d2ba3f86b
Yi-An Chen <yachen@nuvoton.com>
2017-11-24 15:13:23 +0800

    Add SPI flash ID entry for GD25Q128
    Change-Id: I7890324d27cc0a36b5ebcafeeebf110d13b909ad

--- drivers/mtd/devices/m25p80.c
+++ drivers/mtd/devices/m25p80.c
@@ -905,8 +905,9 @@ static const struct spi_device_id m25p_ids[] = {
 	{ "mr25h256", CAT25_INFO(  32 * 1024, 1, 256, 2) },
 
 	/* GigaDevice */
-	{ "gd25q32", INFO(0xc84016, 0, 64 * 1024,  64, SECT_4K) },
-	{ "gd25q64", INFO(0xc84017, 0, 64 * 1024, 128, SECT_4K) },
+	{ "gd25q32",  INFO(0xc84016, 0, 64 * 1024,  64, SECT_4K) },
+	{ "gd25q64",  INFO(0xc84017, 0, 64 * 1024, 128, SECT_4K) },
+	{ "gd25q128", INFO(0xc84018, 0, 64 * 1024, 256, SECT_4K) },
 
 	/* Intel/Numonyx -- xxxs33b */
 	{ "160s33b",  INFO(0x898911, 0, 64 * 1024,  32, 0) },
