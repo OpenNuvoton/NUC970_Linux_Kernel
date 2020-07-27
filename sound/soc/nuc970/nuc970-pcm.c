17d3fa83e7e354c621ddc08c95746311e250d635
Yi-An Chen <yachen@nuvoton.com>
2020-07-27 18:07:01 +0800

    Increase audio buffer size
    This is to avoid audio stuttering while system's setting busy
    
    Change-Id: I1085c3df57fa266091d5d7714c1148ed808e45e7

--- sound/soc/nuc970/nuc970-pcm.c
+++ sound/soc/nuc970/nuc970-pcm.c
@@ -34,9 +34,9 @@ static const struct snd_pcm_hardware nuc970_pcm_hardware = {
 	.formats		= SNDRV_PCM_FMTBIT_S16_LE,
 	.channels_min		= 1,
 	.channels_max		= 2,
-	.buffer_bytes_max	= 4*1024,
+	.buffer_bytes_max	= 64*1024,
 	.period_bytes_min	= 1*1024,
-	.period_bytes_max	= 4*1024,
+	.period_bytes_max	= 16*1024,
 	.periods_min		= 1,
 	.periods_max		= 1024,
 };
@@ -303,7 +303,7 @@ static int nuc970_dma_new(struct snd_soc_pcm_runtime *rtd)
 		card->dev->coherent_dma_mask = DMA_BIT_MASK(32);
 
 	snd_pcm_lib_preallocate_pages_for_all(pcm, SNDRV_DMA_TYPE_DEV,
-		card->dev, 4 * 1024, (4 * 1024) - 1);
+		card->dev, 16 * 1024, (16 * 1024) - 1);
 
 	return 0;
 }
